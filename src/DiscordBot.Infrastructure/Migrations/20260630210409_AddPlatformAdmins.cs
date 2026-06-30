6f2036c87e7ffb6a8453408ada69022de3a92c16
macbook <alsenanimohammed1@gmail.com>
2026-07-01 00:05:28 +0300

    Fix PlatformAdmins migration

---
+++ src/DiscordBot.Infrastructure/Migrations/20260630210409_AddPlatformAdmins.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscordBot.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlatformAdmins : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
